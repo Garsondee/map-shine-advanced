17e6255a53e04b04350a85b5e962b7c3fb3b2c56
Ingram Blakelock <garsondee@gmail.com>
2025-11-28 21:28:57 +0000

    GNU Terry Pratchett.

--- scripts/module.js
+++ scripts/module.js
@@ -34,6 +34,12 @@ window.MapShine = MapShine;
  */
 Hooks.once('init', async function() {
   info('Initializing...');
+
+  console.log(
+  "%c GNU Terry Pratchett %c \n“A man is not dead while his name is still spoken.”",
+  "background: #4b0082; color: #FFD700; font-weight: bold; padding: 4px 8px; border-radius: 4px;",
+  "color: #888; font-style: italic;"
+);
   
   // Register settings
   sceneSettings.registerSettings();
